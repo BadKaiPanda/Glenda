de0d922705570f926a4f9bdd6be3f5862156b761
Lue Lusten (Webmaster) <webmaster@wittles.co.uk>
2016-12-04 20:58:05 +0000

    Creation of the Reference
    This is the basic reference file nothing special

---
+++ src/java/uk/wittles/glenda/Reference.java
@@ -0,0 +1,11 @@
+package uk.wittles.glenda;
+
+public class Reference {
+
+	public static final String MODID = "glenda";
+	public static final String NAME = "Glenda";
+	public static final String VERSION = "0.0.0.1";
+	public static final String SERVER_PROXY_CLASS = "uk.wittles.glenda.proxy.CommonProxy";
+	public static final String CLIENT_PROXY_CLASS = "uk.wittles.glenda.proxy.ClientProxy";
+	
+}
